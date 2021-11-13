273e35a7252f765135efccbddf612c1704f6ee10
Sarvesh Sharma <sssarvesh1999@gmail.com>
2021-11-13 09:52:05 +0530

    Increase font size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                     An Enthusiastic Full Stack Developer moulded  and shaped by masai school's rigorous training program ,
                     with a passion for problem solving and to work upon new ideas.Possessing a knack to learn new technologies and to integrate them forming meaningful projects.
                     Strong in ReactJS, Data Structures, Algorithm, Operating System, C, C++, Javascript, HTML, CSS , NodeJS, MongoDB, ExpressJS, mySQL.
-                    Challenges and new problem exite me to the core.
+                   Good in Core Computer Science Subjects Like Computer Network,DBMS,Operating System. Challenges and new problem exite me to the core.
                Passionately looking to start a career as a software developer.Always ready to adapt to new situations.
            </div>
             
