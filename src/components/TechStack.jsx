2f1f4e9bcf49660c4fbca6bffec5ea398c9a49e3
Sarvesh Sharma <sssarvesh1999@gmail.com>
2021-11-13 21:14:20 +0530

    Changes made

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -19,29 +19,29 @@ const TechStack = () => {
             <h1 style={{textAlign:"center",color:"#5E5757"}}>TECH STACK</h1>
             <div className={styles.tech__stack}>
                 <div >
-                    <FaNodeJs />
+                    <FaNodeJs className={styles.sicon}/>
 
                     {/* <img src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"  alt="nodejs" /> */}
                     <p>NodeJS</p> 
                     
                 </div>
                 <div>
-                    <SiMongodb  style={{width:"10vw",height:"10vh"}}/>
+                    <SiMongodb  className={styles.sicon}/>
                     {/* <img src="https://cdn.iconscout.com/icon/free/png-512/mongodb-5-1175140.png" alt="mongoDB"/> */}
                     <p>MongoDB</p>
                 </div>
                 <div> 
-                    <FaReact style={{width:"10vw",height:"10vh"}}/>
+                    <FaReact className={styles.sicon}/>
                     {/* <img src={`/logo192.png`} alt="react" /> */}
                     <p>ReactJS</p>
                 </div>
                 <div>
-                    <SiJavascript style={{width:"10vw",height:"10vh"}} />
+                    <SiJavascript className={styles.sicon} />
                     {/* <img src="https://cdn.iconscout.com/icon/free/png-256/javascript-2752148-2284965.png" alt="js" /> */}
                     <p>Javascript</p>
                 </div>
                 <div style={{ color: "black" }}>
-                    <FaAws style={{width:"10vw",height:"10vh"}} />
+                    <FaAws className={styles.sicon} />
 
                     {/* <AWSIcon  /> */}
 
@@ -49,38 +49,38 @@ const TechStack = () => {
                     <p>Aws</p>
                 </div>
                 <div>
-                    <SiHtml5 style={{width:"10vw",height:"10vh"}} />
+                    <SiHtml5 className={styles.sicon} />
                     {/* <img src="https://image.flaticon.com/icons/png/512/919/919827.png" alt="HTML" /> */}
                     <p>HTML</p>
         
                 </div>
                 <div>
-                    <SiCss3 style={{width:"10vw",height:"10vh"}} />
+                    <SiCss3 className={styles.sicon} />
                     {/* <img src="https://cdn-icons-png.flaticon.com/512/919/919826.png" alt="CSS" /> */}
                     <p>CSS</p>
                 </div>
                 <div>
-                    <MdOutlineSync style={{width:"10vw",height:"10vh"}} />
+                    <MdOutlineSync className={styles.sicon} />
 
                     {/* <img src="https://image.flaticon.com/icons/png/128/2857/2857406.png" alt="data" /> */}
                     <p>Data Structures</p>
                 </div>
-                <div><SiExpress style={{ width: "10vw", height: "10vh" }} />
+                <div><SiExpress className={styles.sicon}/>
                     <p>Express</p>
                 </div>
                 <div>
-                    <SiMysql  style={{width:"10vw",height:"10vh"}} />
+                    <SiMysql  className={styles.sicon} />
                     {/* <img src="https://image.flaticon.com/icons/png/512/919/919827.png" alt="HTML" /> */}
                     <p>MySql</p>
         
                 </div>
                 <div>
-                    <SiGit  style={{width:"10vw",height:"10vh"}} />
+                    <SiGit  className={styles.sicon}/>
                     {/* <img src="https://cdn-icons-png.flaticon.com/512/919/919826.png" alt="CSS" /> */}
                     <p>Git</p>
                 </div>
                 <div>
-                    <SiNpm style={{width:"10vw",height:"10vh"}} />
+                    <SiNpm className={styles.sicon} />
 
                     {/* <img src="https://image.flaticon.com/icons/png/128/2857/2857406.png" alt="data" /> */}
                     <p>Npm</p>
